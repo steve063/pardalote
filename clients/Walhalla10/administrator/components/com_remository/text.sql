e91358f736da43e011fc5aa3c2dba2268bb6fae1
Steve Kirkbright <77030506+steve063@users.noreply.github.com>
2021-02-11 15:05:53 +1100

    removed walhalla

--- clients/Walhalla10/administrator/components/com_remository/text.sql
+++
@@ -1,8 +0,0 @@
-						CREATE TABLE IF NOT EXISTS `#__downloads_text` (
-						  `id` int NOT NULL auto_increment,
-						  `fileid` int NOT NULL default 0,
-						  `filetext` longtext NOT NULL,
-							UNIQUE KEY `id` (`id`),
-							KEY `fileid` (`fileid`),
-							FULLTEXT `words` (`filetext`)
-							)  ENGINE=MyISAM;
